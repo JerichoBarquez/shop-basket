b0b2406eb64e22b87900aaa35f20b395c8c3880a
JerichoBarquez <66514653+JerichoBarquez@users.noreply.github.com>
2020-06-11 00:51:30 +0800

    Update ShoppingBasketApplication.java

--- src/main/java/com/project/shopping/ShoppingBasketApplication.java
+++ src/main/java/com/project/shopping/ShoppingBasketApplication.java
@@ -1,25 +1,13 @@
 package com.project.shopping;
 
-import java.nio.charset.StandardCharsets;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.templatemode.TemplateMode;
+
 
 @SpringBootApplication
 public class ShoppingBasketApplication {
 	
-	@Bean
-    	public SpringResourceTemplateResolver htmlTemplateResolver(){
-		SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
-		emailTemplateResolver.setPrefix("classpath:/templates/");
-		emailTemplateResolver.setSuffix(".html");
-		emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
-		emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        return emailTemplateResolver;
-    }
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingBasketApplication.class, args);
