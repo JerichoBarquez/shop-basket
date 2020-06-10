f345930909820457e3a1e2ae5392294243d3d36a
JerichoBarquez <66514653+JerichoBarquez@users.noreply.github.com>
2020-06-11 00:03:40 +0800

    Update ShoppingBasketApplication.java

--- src/main/java/com/project/shopping/ShoppingBasketApplication.java
+++ src/main/java/com/project/shopping/ShoppingBasketApplication.java
@@ -5,6 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShoppingBasketApplication {
+	
+	@Bean
+    	public SpringResourceTemplateResolver htmlTemplateResolver(){
+        SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
+        emailTemplateResolver.setPrefix("classpath:/templates/");
+        emailTemplateResolver.setSuffix(".html");
+        emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        return emailTemplateResolver;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingBasketApplication.class, args);
