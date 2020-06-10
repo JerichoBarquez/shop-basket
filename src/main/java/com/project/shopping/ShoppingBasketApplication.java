cb2ea7652adfbb39eb3599d00fe2793990efde1e
JerichoBarquez <66514653+JerichoBarquez@users.noreply.github.com>
2020-06-11 00:05:43 +0800

    Update ShoppingBasketApplication.java

--- src/main/java/com/project/shopping/ShoppingBasketApplication.java
+++ src/main/java/com/project/shopping/ShoppingBasketApplication.java
@@ -1,7 +1,12 @@
 package com.project.shopping;
 
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @SpringBootApplication
 public class ShoppingBasketApplication {
