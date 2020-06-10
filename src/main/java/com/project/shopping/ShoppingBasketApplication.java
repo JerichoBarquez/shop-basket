1c5fa0f567185591e23d5df026d54cd4e8ee1df3
JerichoBarquez <66514653+JerichoBarquez@users.noreply.github.com>
2020-06-11 00:10:48 +0800

    Update ShoppingBasketApplication.java

--- src/main/java/com/project/shopping/ShoppingBasketApplication.java
+++ src/main/java/com/project/shopping/ShoppingBasketApplication.java
@@ -14,7 +14,7 @@ public class ShoppingBasketApplication {
 	@Bean
     	public SpringResourceTemplateResolver htmlTemplateResolver(){
 		SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
-		emailTemplateResolver.setPrefix("classpath:/templates");
+		emailTemplateResolver.setPrefix("classpath:/templates/");
 		emailTemplateResolver.setSuffix(".html");
 		emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
 		emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
